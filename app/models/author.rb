bb8f0c6c0bf85f0a95afa74fe37978980eb3c1c6
thexyman <makdaa1@gmail.com>
2018-09-25 10:51:27 +0100

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,14 @@
 class Author < ActiveRecord::Base
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :phone_number, length: { is: 10 }
+
 end
+
+
+
+  # def title_contains_click_bait
+  #   if self.title && ["Won't Believe", "Secret", "Top", "Guess"].none? { |phrase| self.title.include?(phrase) }
+  #     errors.add(:title, "Title must contain a click baity word!")
+  #   end
+  # end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,29 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :is_clickbait?
+
+  CLICKBAIT_PATTERNS = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top [0-9]*/i,
+    /Guess/i
+  ]
+
+  def is_clickbait?
+    if CLICKBAIT_PATTERNS.none? { |str| str.match(self.title)  }
+      errors.add(:title, "must be clickbait")
+    end
+  end
+
+  # def is_clickbait?
+  #   array = ["Won't Believe", "Secret", "Top", "Guess"]
+  #   if array.none? {|str| self.title.include?(str)}
+  #     byebug
+  #     errors.add(:title, "must be clickbait")
+  #   end
+  # end
+
 end
